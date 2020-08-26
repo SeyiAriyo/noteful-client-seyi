e53245871fe9b41df1dbabf60c74f1d6502df8c3
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-26 14:32:20 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://shielded-escarpment-41988.herokuapp.com/api/`,
+  API_ENDPOINT: `https://blooming-temple-94696.herokuapp.com/`,
   API_KEY: '910237e9-95fd-4ecf-b17b-4af6605a1f01',
 }
