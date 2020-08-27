9479013664e62e76f98d9093ef76549a9e76ebf5
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-27 17:34:15 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://desolate-tor-99506.herokuapp.com/`,
+  API_ENDPOINT: `https://desolate-tor-99506.herokuapp.com/api`,
   API_KEY: '910237e9-95fd-4ecf-b17b-4af6605a1f01',
 }
